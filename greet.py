00f96b84f1bcc97b0e5673fa2ebdb44f4d4dc71e
vijay <vijaypopuri2021@gmail.com>
2025-01-25 07:58:31 +0530

    firstFlaskChngs

---
+++ greet.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app=Flask(__name__)
+api=Api(app)
+
+class Hello(Resource):
+    def get(self,name):
+        #return {f'Hello GoodMorning {name}'}
+        return {"message": f"Hello, {name}! GoodMorning"}
+    
+api.add_resource(Hello,'/hello/<string:name>')
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
+#mor-pyth,java
+#evg spring, lan,g crick
